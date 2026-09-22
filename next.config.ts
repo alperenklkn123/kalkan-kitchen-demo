709fe3874234376012d1ab099e1eff1c6f288dec
alperenklkn123 <crazyboykeles07@gmail.com>
2026-09-22 12:31:56 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
+
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+const basePath = isGitHubPages ? "/kalkan-kitchen-demo" : "";
+
 const config: NextConfig = {
   reactStrictMode: true,
   devIndicators: false,
   turbopack: { root: process.cwd() },
+
+  ...(isGitHubPages
+    ? {
+        output: "export",
+        basePath,
+        images: {
+          unoptimized: true,
+        },
+        trailingSlash: true,
+      }
+    : {}),
 };
+
 export default config;
